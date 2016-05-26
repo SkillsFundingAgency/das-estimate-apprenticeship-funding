cc4c331f528a3989921b4694c532440aba0cdf53
Simon Bull <simon.bull@taurumconsulting.com>
2016-05-26 16:09:54 +0100

    Change to gov.uk styling

---
+++ src/SFA.DAS.ForecastingTool.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Optimization;
+
+namespace SFA.DAS.ForecastingTool.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/assets/javascripts/govuk-template.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/assets/stylesheets/govuk-template.css",
+                      "~/assets/stylesheets/fonts.css",
+                      "~/Content/Site.css"
+                      ));
+        }
+    }
+}

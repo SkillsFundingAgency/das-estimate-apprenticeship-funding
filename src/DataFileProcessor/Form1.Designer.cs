9f9195fbd30b4f1f2eb9672c1e96f734b4f46191
Simon Bull <simon.bull@taurumconsulting.com>
2016-05-31 10:54:40 +0100

    Add tool to parse data for standards

---
+++ src/DataFileProcessor/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace DataFileProcessor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rdnFormatPoc = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOutputDir = new System.Windows.Forms.TextBox();
+            this.btnBrowseOutputDir = new System.Windows.Forms.Button();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(12, 25);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInput.Size = new System.Drawing.Size(760, 468);
+            this.txtInput.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 505);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Format";
+            // 
+            // rdnFormatPoc
+            // 
+            this.rdnFormatPoc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.rdnFormatPoc.AutoSize = true;
+            this.rdnFormatPoc.Checked = true;
+            this.rdnFormatPoc.Location = new System.Drawing.Point(57, 503);
+            this.rdnFormatPoc.Name = "rdnFormatPoc";
+            this.rdnFormatPoc.Size = new System.Drawing.Size(45, 17);
+            this.rdnFormatPoc.TabIndex = 3;
+            this.rdnFormatPoc.TabStop = true;
+            this.rdnFormatPoc.Text = "PoC";
+            this.rdnFormatPoc.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(136, 504);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Output dir";
+            // 
+            // txtOutputDir
+            // 
+            this.txtOutputDir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutputDir.Location = new System.Drawing.Point(195, 500);
+            this.txtOutputDir.Name = "txtOutputDir";
+            this.txtOutputDir.Size = new System.Drawing.Size(541, 20);
+            this.txtOutputDir.TabIndex = 5;
+            // 
+            // btnBrowseOutputDir
+            // 
+            this.btnBrowseOutputDir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseOutputDir.Location = new System.Drawing.Point(742, 500);
+            this.btnBrowseOutputDir.Name = "btnBrowseOutputDir";
+            this.btnBrowseOutputDir.Size = new System.Drawing.Size(30, 20);
+            this.btnBrowseOutputDir.TabIndex = 6;
+            this.btnBrowseOutputDir.Text = "...";
+            this.btnBrowseOutputDir.UseVisualStyleBackColor = true;
+            this.btnBrowseOutputDir.Click += new System.EventHandler(this.btnBrowseOutputDir_Click);
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnProcess.Location = new System.Drawing.Point(12, 526);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(760, 23);
+            this.btnProcess.TabIndex = 7;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnProcess;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.btnBrowseOutputDir);
+            this.Controls.Add(this.txtOutputDir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rdnFormatPoc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(640, 480);
+            this.Name = "Form1";
+            this.Text = "Data File Processor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rdnFormatPoc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOutputDir;
+        private System.Windows.Forms.Button btnBrowseOutputDir;
+        private System.Windows.Forms.Button btnProcess;
+    }
+}
+
---
+++ src/DataFileProcessor/PocStandard.cs
@@ -0,0 +1,11 @@
+﻿namespace DataFileProcessor
+{
+    public class PocStandard
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public int NotionalEndLevel { get; set; }
+        public int BandNumber { get; set; }
+        public int Price { get; set; }
+    }
+}

7115abfbd5c5e42b239239511f569aa886d19a97
Simon Bull <simon.bull@taurumconsulting.com>
2016-06-06 13:33:04 +0100

    Skip english fraction if PAYE less that £3m. update change links to include english fraction

--- src/SFA.DAS.ForecastingTool.Web/Infrastructure/Routing/PostToUrlRedirectHandler.cs
+++ src/SFA.DAS.ForecastingTool.Web/Infrastructure/Routing/PostToUrlRedirectHandler.cs
@@ -14,12 +14,23 @@ namespace SFA.DAS.ForecastingTool.Web.Infrastructure.Routing
             if (form.AllKeys.Contains("paybillSubmit"))
             {
                 var paybillEntry = GetFormValue(form, "paybill", "0");
-                Redirect(context, $"{GetUrlToSegment(currentUrl, 1)}{paybillEntry}");
+                if (PaybillIsEligibleForLevy(paybillEntry))
+                {
+                    Redirect(context, $"{GetUrlToSegment(currentUrl, 1)}{paybillEntry}");
+                }
+                else
+                {
+                    Redirect(context, $"{GetUrlToSegment(currentUrl, 1)}{paybillEntry}/1");
+                }
             }
             else if (form.AllKeys.Contains("englishFractionSubmit"))
             {
                 var englishFractionEntry = GetFormValue(form, "englishFraction", "0");
                 Redirect(context, $"{GetUrlToSegment(currentUrl, 2)}{englishFractionEntry}");
+            }
+            else if (form.AllKeys.Contains("englishFractionSkip"))
+            {
+                Redirect(context, $"{GetUrlToSegment(currentUrl, 2)}100");
             }
             else if (form.AllKeys.Contains("trainingCourseSubmit"))
             {
@@ -52,6 +63,15 @@ namespace SFA.DAS.ForecastingTool.Web.Infrastructure.Routing
         {
             var value = form[name];
             return string.IsNullOrEmpty(value) ? defaultValue : value;
+        }
+        private bool PaybillIsEligibleForLevy(string paybillEntry)
+        {
+            int paybill;
+            if (int.TryParse(paybillEntry, out paybill))
+            {
+                return paybill >= 3000000;
+            }
+            return false;
         }
     }
 }

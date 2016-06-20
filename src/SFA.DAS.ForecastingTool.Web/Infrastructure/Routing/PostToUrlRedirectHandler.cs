78c34fe05e025b489e8daff2f084e82a873f90ac
Simon Bull <simon.bull@taurumconsulting.com>
2016-06-20 09:15:39 +0100

    Refactor PostToUrlRedirectHandler

--- src/SFA.DAS.ForecastingTool.Web/Infrastructure/Routing/PostToUrlRedirectHandler.cs
+++ src/SFA.DAS.ForecastingTool.Web/Infrastructure/Routing/PostToUrlRedirectHandler.cs
@@ -1,83 +1,121 @@
 ﻿using System;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Web;
-using System.Web.Mvc;
 
 namespace SFA.DAS.ForecastingTool.Web.Infrastructure.Routing
 {
     public class PostToUrlRedirectHandler : IHttpHandler
     {
+        private readonly ActionHandlerMapping[] _actionHandlers;
+
+        public PostToUrlRedirectHandler()
+        {
+            _actionHandlers = new[]
+            {
+                new ActionHandlerMapping {ActionKey = "paybillSubmit", Handler = PaybillSubmit},
+                new ActionHandlerMapping {ActionKey = "englishFractionSubmit", Handler = EnglishFractionSubmit},
+                new ActionHandlerMapping {ActionKey = "englishFractionSkip", Handler = EnglishFractionSkip},
+                new ActionHandlerMapping {ActionKey = "trainingCourseSubmit", Handler = TrainingCourseSubmit},
+                new ActionHandlerMapping {ActionKey = "trainingCourseSkip", Handler = TrainingCourseSkip},
+                new ActionHandlerMapping {ActionKey = "trainingCourseAdd", Handler = TrainingCourseAdd},
+                new ActionHandlerMapping {ActionKey = "trainingCourseDelete", Handler = TrainingCourseDelete}
+            };
+        }
+
         public void ProcessRequest(HttpContext context)
         {
             var form = context.Request.Form;
+            var handler = GetHandler(form);
+            if (handler == null)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return;
+            }
+
             var currentUrl = context.Request.Url;
-            if (form.AllKeys.Contains("paybillSubmit"))
-            {
-                var paybillEntry = GetFormValue(form, "paybill", "0");
-                decimal paybill;
-                if (decimal.TryParse(paybillEntry, out paybill))
-                {
-                    paybillEntry = Math.Floor(paybill).ToString();
-                }
-                if (PaybillIsEligibleForLevy(paybillEntry))
-                {
-                    Redirect(context, $"{currentUrl.GetUrlToSegment(1)}{EncodeFormEntryForUrl(paybillEntry)}/");
-                }
-                else
-                {
-                    Redirect(context, $"{currentUrl.GetUrlToSegment(1)}{EncodeFormEntryForUrl(paybillEntry)}/NA/");
-                }
-            }
-            else if (form.AllKeys.Contains("englishFractionSubmit"))
-            {
-                var englishFractionEntry = GetFormValue(form, "englishFraction", "0");
-                decimal englishFraction;
-                if (decimal.TryParse(englishFractionEntry, out englishFraction))
-                {
-                    englishFractionEntry = Math.Floor(englishFraction).ToString();
-                }
-                Redirect(context, $"{currentUrl.GetUrlToSegment(2)}{EncodeFormEntryForUrl(englishFractionEntry)}/");
-            }
-            else if (form.AllKeys.Contains("englishFractionSkip"))
-            {
-                Redirect(context, $"{currentUrl.GetUrlToSegment(2)}100/");
-            }
-            else if (form.AllKeys.Contains("trainingCourseSubmit"))
-            {
-                var cohorstSegment = GetCohortsUrlSegment(form);
-                if (string.IsNullOrEmpty(cohorstSegment))
-                {
-                    cohorstSegment = "noselection";
-                }
-                Redirect(context, $"{currentUrl.GetUrlToSegment(3)}{EncodeFormEntryForUrl(cohorstSegment)}/12/");
-            }
-            else if (form.AllKeys.Contains("trainingCourseSkip"))
-            {
-                Redirect(context, $"{currentUrl.GetUrlToSegment(3)}0x0/12/");
-            }
-            else if (form.AllKeys.Contains("trainingCourseAdd"))
-            {
-                var cohorstSegment = GetCohortsUrlSegment(form);
-                Redirect(context, $"{currentUrl.GetUrlToSegment(3)}{EncodeFormEntryForUrl(cohorstSegment)}/");
-            }
-            else if (form.AllKeys.Any(k => k.StartsWith("trainingCourseDelete")))
-            {
-                var cohortToDelete = form.AllKeys.First(k => k.StartsWith("trainingCourseDelete")).Substring(20);
-                var originalCohorstSegment = GetCohortsUrlSegment(form);
-                var fixedCohortSegment = originalCohorstSegment.Replace(cohortToDelete, "").Replace("__", "_");
-                if (fixedCohortSegment.StartsWith("_"))
-                {
-                    fixedCohortSegment = fixedCohortSegment.Substring(1);
-                }
-                Redirect(context, $"{currentUrl.GetUrlToSegment(3)}{EncodeFormEntryForUrl(fixedCohortSegment)}/");
-            }
+            handler.Handler.Invoke(context, form, currentUrl);
         }
 
 
         public bool IsReusable { get; } = false;
 
+
+        private void PaybillSubmit(HttpContext context, NameValueCollection form, Uri currentUrl)
+        {
+            var paybillEntry = GetFormValue(form, "paybill", "0");
+            decimal paybill;
+            if (decimal.TryParse(paybillEntry, out paybill))
+            {
+                paybillEntry = Math.Floor(paybill).ToString();
+            }
+            if (PaybillIsEligibleForLevy(paybillEntry))
+            {
+                Redirect(context, $"{currentUrl.GetUrlToSegment(1)}{EncodeFormEntryForUrl(paybillEntry)}/");
+            }
+            else
+            {
+                Redirect(context, $"{currentUrl.GetUrlToSegment(1)}{EncodeFormEntryForUrl(paybillEntry)}/NA/");
+            }
+        }
+        private void EnglishFractionSubmit(HttpContext context, NameValueCollection form, Uri currentUrl)
+        {
+            var englishFractionEntry = GetFormValue(form, "englishFraction", "0");
+            decimal englishFraction;
+            if (decimal.TryParse(englishFractionEntry, out englishFraction))
+            {
+                englishFractionEntry = Math.Floor(englishFraction).ToString();
+            }
+            Redirect(context, $"{currentUrl.GetUrlToSegment(2)}{EncodeFormEntryForUrl(englishFractionEntry)}/");
+        }
+        private void EnglishFractionSkip(HttpContext context, NameValueCollection form, Uri currentUrl)
+        {
+            Redirect(context, $"{currentUrl.GetUrlToSegment(2)}100/");
+        }
+        private void TrainingCourseSubmit(HttpContext context, NameValueCollection form, Uri currentUrl)
+        {
+            var cohorstSegment = GetCohortsUrlSegment(form);
+            if (string.IsNullOrEmpty(cohorstSegment))
+            {
+                cohorstSegment = "noselection";
+            }
+            Redirect(context, $"{currentUrl.GetUrlToSegment(3)}{EncodeFormEntryForUrl(cohorstSegment)}/12/");
+        }
+        private void TrainingCourseSkip(HttpContext context, NameValueCollection form, Uri currentUrl)
+        {
+            Redirect(context, $"{currentUrl.GetUrlToSegment(3)}0x0/12/");
+        }
+        private void TrainingCourseAdd(HttpContext context, NameValueCollection form, Uri currentUrl)
+        {
+            var cohorstSegment = GetCohortsUrlSegment(form);
+            Redirect(context, $"{currentUrl.GetUrlToSegment(3)}{EncodeFormEntryForUrl(cohorstSegment)}/");
+        }
+        private void TrainingCourseDelete(HttpContext context, NameValueCollection form, Uri currentUrl)
+        {
+            var cohortToDelete = form.AllKeys.First(k => k.StartsWith("trainingCourseDelete")).Substring(20);
+            var originalCohorstSegment = GetCohortsUrlSegment(form);
+            var fixedCohortSegment = originalCohorstSegment.Replace(cohortToDelete, "").Replace("__", "_");
+            if (fixedCohortSegment.StartsWith("_"))
+            {
+                fixedCohortSegment = fixedCohortSegment.Substring(1);
+            }
+            Redirect(context, $"{currentUrl.GetUrlToSegment(3)}{EncodeFormEntryForUrl(fixedCohortSegment)}/");
+        }
+
+
+        private ActionHandlerMapping GetHandler(NameValueCollection form)
+        {
+            foreach (var handler in _actionHandlers)
+            {
+                if (form.AllKeys.Any(k => k.StartsWith(handler.ActionKey)))
+                {
+                    return handler;
+                }
+            }
+            return null;
+        }
         private void Redirect(HttpContext context, string relativeUrl)
         {
             var inboundUri = context.Request.Url;
@@ -85,13 +123,11 @@ namespace SFA.DAS.ForecastingTool.Web.Infrastructure.Routing
             var redirectUri = new Uri(baseUri, relativeUrl);
             context.Response.Redirect(redirectUri.ToString());
         }
-        
         private string GetFormValue(NameValueCollection form, string name, string defaultValue = "")
         {
             var value = form[name];
             return string.IsNullOrEmpty(value) ? defaultValue : value;
         }
-
         private string EncodeFormEntryForUrl(string entry)
         {
             var encoded = HttpUtility.UrlEncode(entry);
@@ -101,7 +137,6 @@ namespace SFA.DAS.ForecastingTool.Web.Infrastructure.Routing
             }
             return encoded;
         }
-
         private bool PaybillIsEligibleForLevy(string paybillEntry)
         {
             long paybill;
@@ -111,7 +146,6 @@ namespace SFA.DAS.ForecastingTool.Web.Infrastructure.Routing
             }
             return false;
         }
-
         private string GetCohortsUrlSegment(NameValueCollection form)
         {
             var cohortsEntry = GetFormValue(form, "cohorts", "0").Split(',');
@@ -120,7 +154,7 @@ namespace SFA.DAS.ForecastingTool.Web.Infrastructure.Routing
 
             var segmentbuilder = new StringBuilder();
 
-            for (var i = 0; i < standardSelection.Length; i ++)
+            for (var i = 0; i < standardSelection.Length; i++)
             {
                 if (standardSelection[i] == "noselection")
                 {
@@ -135,5 +169,11 @@ namespace SFA.DAS.ForecastingTool.Web.Infrastructure.Routing
 
             return segmentbuilder.ToString();
         }
+    }
+
+    internal class ActionHandlerMapping
+    {
+        public string ActionKey { get; set; }
+        public Action<HttpContext, NameValueCollection, Uri> Handler { get; set; }
     }
 }

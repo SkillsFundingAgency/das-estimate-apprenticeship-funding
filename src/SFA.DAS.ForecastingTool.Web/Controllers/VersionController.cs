e8b27e1cda346fd8eff16bfca9ad4ef2a10bf33d
Scott Cowan <info@scottcowan.org>
2016-12-06 16:01:59 +0000

    adding version controller

---
+++ src/SFA.DAS.ForecastingTool.Web/Controllers/VersionController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Web.Http;
+
+namespace SFA.DAS.ForecastingTool.Web.Controllers
+{
+    public sealed class VersionController : ApiController
+    {
+        // GET: api/Version
+        public VersionInformation Get()
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string assemblyInformationalVersion = fileVersionInfo.ProductVersion;
+            return new VersionInformation
+            {
+                Version = assemblyInformationalVersion,
+                AssemblyVersion = version
+            };
+        }
+
+        public class VersionInformation
+        {
+            public string BuildId { get; set; }
+
+            public string Version { get; set; }
+
+            public string AssemblyVersion { get; set; }
+        }
+    }
+}
